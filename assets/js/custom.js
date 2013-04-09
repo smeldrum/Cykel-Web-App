4dc24d542c7e6423af116386885646cc453c9986
adempsey <amd0331@gmail.com>
2013-04-08 22:20:04 -0400

    added ability to go back to registration div from signin div

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
 	$('#btn_signin').click(function() {
-		$('#mainstage').fadeOut('slow', function() {
-			$('#signin').fadeIn('slow');
+		$('#mainstage, #btn_signin').fadeOut('slow', function() {
+			$('#signin, #btn_register').fadeIn('slow');
+		});
+	});
+	
+	$('#btn_register').click(function() {
+		$('#signin, #btn_register').fadeOut('slow', function() {
+			$('#mainstage, #btn_signin').fadeIn('slow');
 		});
 	});
 });

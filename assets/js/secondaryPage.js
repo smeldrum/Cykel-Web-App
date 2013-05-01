e9bf38e4c8b4b0e88289acbda9a6b23317069e7e
luke.dahill@gmail.com <Luke@resnet133-37.medford.tufts.edu>
2013-05-01 15:48:25 -0400

    Edited secondaryPage.Renders google map on canvas on Map click.

---
+++ assets/js/secondaryPage.js
@@ -0,0 +1,56 @@
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+
+
+function renderMap(){
+	$('.row').append('<div id="map_canvas" width="600" height="200"></div>');
+	//$('.row').append('<div id="directions" width="300" height="200"></div>');
+	initialize();
+	//<div id="directions" width="300" height="200"></div>
+	$(document).ready(function() {
+        document.title = 'blah';
+	});   	
+}
+//initialize() sets the google map canvas, centers it, and calls 
+//functions to set markers and create polyline
+function initialize(){
+  console.log("CALLED");
+  mapOptions = {
+  center: new google.maps.LatLng(42.330497742, -71.095794678),
+  zoom: 12,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+  try {request = new XMLHttpRequest();}
+  catch (IE) {
+  	try {request = new ActiveXObject("Msxml2.XMLHTTP");}
+	catch (notIE7) {
+		try {request = new ActiveXObject("Microsoft.XMLHTTP");}
+		catch (ex) { request = null;}
+  	}
+  }
+  if (request == null) {
+ 		return "Error creating request object --Ajax not supported?";	  		
+  }	
+  direct();
+}
+
+function direct(){
+ directionsDisplay = new google.maps.DirectionsRenderer();
+ directionsDisplay.setMap(map);
+ directionsDisplay.setPanel(document.getElementById("directions"));
+ var start = "Tufts University, 419 Boston Avenue, Medford, MA, United States";
+ var end = "Fresh Pond Parkway, Cambridge, MA";
+ var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.BICYCLING,
+    unitSystem: google.maps.UnitSystem.METRIC,
+    avoidHighways: true
+ };
+ directionsService.route(request, function(result, status) {
+ 	if (status == google.maps.DirectionsStatus.OK) {
+    	directionsDisplay.setDirections(result);
+ 	}
+ });
+}
